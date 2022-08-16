6e2dc3e2fc4a36df72b39911034b83f091cd010a
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-08-16 17:30:08 +0300

    08-09 Settings Box Change Colors Option - Save Colors To Local Storage & Remove and Add Active Class

--- js/master.js
+++ js/master.js
@@ -1,3 +1,11 @@
+// Check If There's Local Storage Color Option
+let mainColors = localStorage.getItem("color_option");
+
+// If There's Color Item In Local Storage
+if (mainColors !== null) {
+  document.documentElement.style.setProperty("--main-color", mainColors);
+}
+
 // Toggle Spin Class On Icon
 document.querySelector(".toggle-settings .fa-gear").onclick = function () {
   // Toggle Class Fa-spin For Rotation on Self
@@ -6,6 +14,32 @@ document.querySelector(".toggle-settings .fa-gear").onclick = function () {
   // Toggle Class Open On Main Settings Box
   document.querySelector(".settings-box").classList.toggle("open");
 };
+
+// Switch Colors
+const colorsLi = document.querySelectorAll(".colors-list li");
+
+// Loop On All List Items
+colorsLi.forEach((li) => {
+  // Click On Every List Items
+  li.addEventListener("click", (e) => {
+    // Set Color On Root
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+
+    // Set Color On Local Storage
+    localStorage.setItem("color_option", e.target.dataset.color);
+
+    // Remove Active Class From All Childrens
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    // Add Active Class On Self
+    e.target.classList.add("active");
+  });
+});
 
 // Select Landing Page Element
 let landingPage = document.querySelector(".landing-page");
